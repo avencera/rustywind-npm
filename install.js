685234e29fbce16a1944d6cb85157f52bed3d204
Praveen Perera <ppraveen25@gmail.com>
2019-11-25 16:11:41 -0500

    Change install script to not need rustc

--- install.js
+++ install.js
@@ -1,8 +1,8 @@
 let exec = require("child_process").exec;
 
 exec(
-  "curl -LSfs https://japaric.github.io/trust/install.sh | \
-sh -s -- --git avencera/rustywind",
+  "curl -LSfs https://gist.githubusercontent.com/praveenperera/585454c3ef85451b3b92209dd822d4aa/raw/7c05b808ba7e4d47676c43b1c6d3649c64e4fa6e/crate-gh-install.sh\
+  |  sh -s -- --git avencera/rustywind -f",
   (error, stdout, stderr) => {
     console.log(stderr);
   }
