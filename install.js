1b7e974a24c60bd20be584e9f048b7254177f374
Praveen Perera <ppraveen25@gmail.com>
2019-11-26 15:55:26 -0500

    Add install script to  repo

--- install.js
+++ install.js
@@ -1,8 +1,7 @@
 let exec = require("child_process").exec;
 
 exec(
-  "curl -LSfs https://gist.githubusercontent.com/praveenperera/585454c3ef85451b3b92209dd822d4aa/raw/7c05b808ba7e4d47676c43b1c6d3649c64e4fa6e/crate-gh-install.sh\
-  |  sh -s -- --git avencera/rustywind -f --to $(npm bin -g)",
+  "install.sh | sh -s -- --git avencera/rustywind -f --to $(npm bin -g)",
   (error, stdout, stderr) => {
     console.log(stderr);
   }
