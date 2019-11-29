3eb1a88377113fae8063ea0de2a97fe17c6d987d
Praveen Perera <ppraveen25@gmail.com>
2019-11-28 19:56:32 -0500

    Fix path to bin

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const path = require("path");
 
 module.exports.rustyWindPath = path.join(
   __dirname,
-  `../bin/rustywind${process.platform === "win32" ? ".exe" : ""}`
+  `rustywind${process.platform === "win32" ? ".exe" : ""}`
 );
